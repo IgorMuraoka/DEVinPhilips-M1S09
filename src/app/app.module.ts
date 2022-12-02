5217bee869fe379c08961393cc3c62e7cd515a15
Igor Minakawa Muraoka <igormuraoka97@gmail.com>
2022-12-01 23:34:52 -0300

    Add Ex04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCardComponent } from './item-card/item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    NavbarComponent
+    NavbarComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule
