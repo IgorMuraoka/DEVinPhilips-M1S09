c61f9c7249f0f7257459375183c62a4176334565
Igor Minakawa Muraoka <igormuraoka97@gmail.com>
2022-12-01 23:46:26 -0300

    Add Ex05

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ex-item-card',
   templateUrl: './item-card.component.html',
-  styleUrls: ['./item-card.component.css']
+  styleUrls: ['./item-card.component.css'],
 })
 export class ItemCardComponent {
-
+  personagem: any = {
+    nome: 'Sheldon Cooper',
+    imagem: './assets/tbbt.jpg',
+    nomeAtor: 'James Joseph “Jim” Parsons',
+    descricao:
+      'Ator estadunidense mais conhecido por seu trabalho na série de televisão The Big Bang Theory como Sheldon Cooper. Jim já recebeu seis indicações ao Emmy Award na categoria de melhor ator em uma série de comédia, vencendo quatro.',
+  };
 }
